3c438f043a44e568ffe024a0fb097ca9806cc95b
ongvang00 <you@example.com>
2023-05-15 14:47:06 -0400

    asfs

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -2,14 +2,13 @@ public class LibraryApp {
     public static void main(String[] args) {
         LibraryManager<LibraryItem> libraryManager = new LibraryManager<>();
 
+        //Instantiate objects of Book and DVD classes
         DVD dvd1 = new DVD("DVD ABC", 2003, "Director1", 102);
-        Book book1 = new Book("Book ABC", 1913, "Author3", "11342425451");
+        Book book1 = new Book("Book ABC", 1913, "Author1", "11342425451");
 
-        // Add an item to the library
+        //Add and remove items from the library
         libraryManager.addItem(book1);
         libraryManager.addItem(dvd1);
-
-        // Remove an item from the library
         libraryManager.removeItem(dvd1);
 
         // Display the list of items in the library
